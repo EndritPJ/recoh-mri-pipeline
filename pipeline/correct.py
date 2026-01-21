c9d0572f039bb23eef6591ce4141018fee6f99da
EndritPJ <endrit.p@hotmail.co.uk>
2026-01-21 16:38:27 +0000

    shrinking before bias field correction works after upgrading to sitk 2.1.0

--- pipeline/correct.py
+++ pipeline/correct.py
@@ -16,33 +16,37 @@ from pipeline.constants import DirectoryStructure as ds, NIFTI_EXT
 
 def estimate_bias_field(input_volume: NiftiWrapper, mask_volume: NiftiWrapper, fwhm: float = 0.15,
                         iterations: Tuple[int, int, int, int] = (50, 50, 50, 50)) -> (np.ndarray, np.ndarray):
-    # sitk_input_volume = nibabel_to_sitk(input_volume.nii_img)
-    # sitk_mask_volume = nibabel_to_sitk(mask_volume.nii_img)
-    # input_volume_shrink = sitk.Shrink(sitk_input_volume, [2] * sitk_input_volume.GetDimension())
-    # mask_volume_shrink = sitk.Shrink(sitk_mask_volume, [2] * sitk_mask_volume.GetDimension())
-    #
-    # corrector = sitk.N4BiasFieldCorrectionImageFilter()
-    # corrector.SetMaximumNumberOfIterations(iterations)
-    # output = corrector.Execute(sitk.Cast(input_volume_shrink, sitk.sitkFloat32),
-    #                            sitk.Cast(mask_volume_shrink, sitk.sitkUInt8))
-    # log_bias_field = corrector.GetLogBiasFieldAsImage(sitk_input_volume)
-    # bias_field = sitk.Exp(log_bias_field)
-    # corrected = np.asanyarray(sitk_to_nibabel(sitk_input_volume / bias_field).dataobj)
-    #
-    # return bias_field, corrected
-
-    n4 = sitk.N4BiasFieldCorrectionImageFilter()
-    n4.SetBiasFieldFullWidthAtHalfMaximum(fwhm)  # default = 0.15
-    n4.SetMaximumNumberOfIterations(iterations)  # default = 50 x 50 x 50 x 50
-
     sitk_input_volume = nibabel_to_sitk(input_volume.nii_img)
     sitk_mask_volume = nibabel_to_sitk(mask_volume.nii_img)
-    sitk_corrected = n4.Execute(sitk.Cast(sitk_input_volume, sitk.sitkFloat32),
-                                sitk.Cast(sitk_mask_volume, sitk.sitkUInt8))
-    corrected = np.asanyarray(sitk_to_nibabel(sitk_corrected).dataobj)
-    bias_field = np.where(corrected > 0, np.asanyarray(input_volume.nii_img.dataobj) / corrected, 0)
+    input_volume_shrink = sitk.Shrink(sitk_input_volume, [2] * sitk_input_volume.GetDimension())
+    mask_volume_shrink = sitk.Shrink(sitk_mask_volume, [2] * sitk_mask_volume.GetDimension())
+    
+    n4 = sitk.N4BiasFieldCorrectionImageFilter()
+    log.debug(f"Global default number of threads: {sitk.ProcessObject.GetGlobalDefaultNumberOfThreads()}")
+    log.debug(f"N4 bias field number of threads, default: {n4.GetNumberOfThreads()}")
+    n4.SetMaximumNumberOfIterations(iterations)
+    corrected_shrunk = n4.Execute(sitk.Cast(input_volume_shrink, sitk.sitkFloat32),
+                               sitk.Cast(mask_volume_shrink, sitk.sitkUInt8))
+    log_bias_field = n4.GetLogBiasFieldAsImage(sitk_input_volume)
+    bias_field = sitk.Exp(log_bias_field)
+    corrected = np.asanyarray(sitk_to_nibabel(sitk.Cast(sitk_input_volume, sitk.sitkFloat32) / bias_field).dataobj)
+    bias_field_array = np.asanyarray(sitk_to_nibabel(bias_field).dataobj)
+    
+    return bias_field_array, corrected
+    # log.debug(f"Global default number of threads: {sitk.ProcessObject.GetGlobalDefaultNumberOfThreads()}")
+    # n4 = sitk.N4BiasFieldCorrectionImageFilter()
+    # log.debug(f"N4 bias field number of threads, default: {n4.GetNumberOfThreads()}")
+    # n4.SetBiasFieldFullWidthAtHalfMaximum(fwhm)  # default = 0.15
+    # n4.SetMaximumNumberOfIterations(iterations)  # default = 50 x 50 x 50 x 50
 
-    return bias_field, corrected
+    # sitk_input_volume = nibabel_to_sitk(input_volume.nii_img)
+    # sitk_mask_volume = nibabel_to_sitk(mask_volume.nii_img)
+    # sitk_corrected = n4.Execute(sitk.Cast(sitk_input_volume, sitk.sitkFloat32),
+    #                             sitk.Cast(sitk_mask_volume, sitk.sitkUInt8))
+    # corrected = np.asanyarray(sitk_to_nibabel(sitk_corrected).dataobj)
+    # bias_field = np.where(corrected > 0, np.asanyarray(input_volume.nii_img.dataobj) / corrected, 0)
+
+    # return bias_field, corrected
 
 
 def bias_field_correction(series_data: Dict, fwhm: float = 0.15, iterations: Tuple = (10, 10, 10, 10)) -> None:
